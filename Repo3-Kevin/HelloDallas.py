ad6d7bf7fa46d4e64ca4109f94aa8a325fb3423a
wanghaox2020 <79470452+wanghaox2020@users.noreply.github.com>
2024-08-29 12:04:40 -0500

    create a getter func in class

--- Repo3-Kevin/HelloDallas.py
+++ Repo3-Kevin/HelloDallas.py
@@ -1,6 +1,10 @@
 class HelloDallas:
     def __init__(self, address: str, zip: int):
-        self._address = address;
+        self._address = address
         self._zip = zip
 
+    def address(self) -> str:
+        return self._address
 
+    def zip(self) -> int:
+        return self._zip
